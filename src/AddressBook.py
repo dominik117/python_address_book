5bd3a61042ad8027dbe6108c3748e591fafd8ac0
Dominik Bacher <dominikbacher@icloud.com>
2023-09-13 14:53:15 +0200

    First app version

---
+++ src/AddressBook.py
@@ -0,0 +1,67 @@
+import csv
+from Contact import Contact
+
+class AddressBook:
+    FILENAME = "address_book.csv"
+
+    def __init__(self):
+        self.contacts = []
+        self.load_from_file()
+
+    def load_from_file(self):
+        try:
+            with open(self.FILENAME, 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    self.contacts.append(Contact(row[0], row[1], row[2]))
+        except FileNotFoundError:
+            pass
+
+    def save_to_file(self):
+        with open(self.FILENAME, 'w', newline='') as file:
+            writer = csv.writer(file)
+            for contact in self.contacts:
+                writer.writerow([contact.name, contact.email, contact.phone])
+
+    def add(self, contact):
+        self.contacts.append(contact)
+        self.save_to_file()
+
+    def view_all(self):
+        for contact in self.contacts:
+            print(contact)
+
+    def search(self, name):
+        for contact in self.contacts:
+            if contact.name.lower() == name.lower():
+                return contact
+        return None
+
+    def edit(self, name):
+        contact = self.search(name)
+        if contact:
+            print(f"Editing {contact}...")
+            new_name = input("Enter new name or press Enter to skip: ")
+            new_email = input("Enter new email or press Enter to skip: ")
+            new_phone = input("Enter new phone or press Enter to skip: ")
+
+            if new_name:
+                contact.name = new_name
+            if new_email:
+                contact.email = new_email
+            if new_phone:
+                contact.phone = new_phone
+
+            self.save_to_file()
+            print("Contact updated successfully!")
+        else:
+            print("Contact not found!")
+
+    def delete(self, name):
+        contact = self.search(name)
+        if contact:
+            self.contacts.remove(contact)
+            self.save_to_file()
+            print("Contact deleted successfully!")
+        else:
+            print("Contact not found!")
---
+++ src/main.py
@@ -0,0 +1,53 @@
+import csv
+from AddressBook import AddressBook
+from Contact import Contact
+
+def main():
+    address_book = AddressBook()
+    while True:
+        print("\n--- Address Book ---")
+        print("1. Add a new contact")
+        print("2. View all contacts")
+        print("3. Search for a contact")
+        print("4. Edit a contact")
+        print("5. Delete a contact")
+        print("6. Exit")
+
+        choice = input("Choose an option (1-6): ")
+
+        if choice == "1":
+            name = input("Enter the name: ")
+            email = input("Enter the email: ")
+            phone = input("Enter the phone number: ")
+            address_book.add(Contact(name, email, phone))
+            print(f"Contact {name} added successfully!")
+
+        elif choice == "2":
+            address_book.view_all()
+
+        elif choice == "3":
+            name = input("Enter the name to search: ")
+            contact = address_book.search(name)
+            if contact:
+                print(contact)
+            else:
+                print("Contact not found!")
+
+        elif choice == "4":
+            name = input("Enter the name of the contact to edit: ")
+            address_book.edit(name)
+
+        elif choice == "5":
+            name = input("Enter the name of the contact to delete: ")
+            address_book.delete(name)
+
+        elif choice == "6":
+            print("Closing...")
+            break
+
+        else:
+            print("Invalid choice! Please choose a valid option.")
+
+
+if __name__ == "__main__":
+    main()
