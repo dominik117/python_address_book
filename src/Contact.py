b6ebb7c0fe4041f62089b5d177457a1bb4ebff5c
Dominik Bacher <dominikbacher@icloud.com>
2023-09-26 11:08:44 +0200

    Rename contact script

--- src/Contact.py
+++
@@ -1,8 +0,0 @@
-class Contact:
-    def __init__(self, name, email, phone):
-        self.name = name
-        self.email = email
-        self.phone = phone
-
-    def __str__(self):
-        return f"{self.name} | {self.email} | {self.phone}"
